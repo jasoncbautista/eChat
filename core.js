565f5a47ae778d4f40d08d8513c8f442f527f39f
Jason <bjason@gmail.com>
2014-12-08 23:12:19 -0800

    Reactive values

--- core.js
+++ core.js
@@ -1,16 +1,44 @@
 
-
-
 // Very simple chat js system
-var eChat = echat || { Core{}};
-
+var eChat = echat || { Core{}, state:{ entities:{}, fxns: {}}};
 
 // TODO: with a promise
 eChat.Core.sendMsg = function(msg, cbError){
 	// We send a simple msg to the server and get called if we error
 
 	///
-
 };
+
+eChat.Core.RValue = function(initialValue){
+	debugger;
+	var _initialValue = initialValue;
+
+	return {
+		set: function(_value){
+			_initialValue = _value;
+		}, 
+
+		get: function(){
+
+			debugger;
+			return _initialValue;
+
+		}
+	};
+};
+
+eChat.Core.sendMsg()
+
+
+
+var rr  = new  eChat.Core.RValue("wassa");
+
+var callBackForHTMLValue = function(){
+	return rr.get();
+};
+
+
+
+
 
 
