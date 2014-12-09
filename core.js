4586523e5db2602bef4c7d697c42367088048fa5
Jason <bjason@gmail.com>
2014-12-08 23:06:20 -0800

    Core outline

---
+++ core.js
@@ -0,0 +1,16 @@
+
+
+
+// Very simple chat js system
+var eChat = echat || { Core{}};
+
+
+// TODO: with a promise
+eChat.Core.sendMsg = function(msg, cbError){
+	// We send a simple msg to the server and get called if we error
+
+	///
+
+};
+
+
